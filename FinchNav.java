3e738048529b0a794ac854300aa667aba3e370a1
Oksisane <ehsanmasdar@gmail.com>
2013-03-08 12:22:58 -0600

    Cleanup of Code

--- FinchNav.java
+++ FinchNav.java
@@ -1,17 +1,11 @@
-import java.awt.Color;
-import java.awt.Robot;
-import java.util.Scanner;
-
 import edu.cmu.ri.createlab.terk.robot.finch.Finch;
-import edu.cmu.ri.createlab.video.VideoHelper;
-import edu.cmu.ri.createlab.video.VideoPlayer;
+
 
 public class FinchNav {
 
 	public static void main(String args[]) throws InterruptedException {
 		Finch myFinch = null;
 		myFinch = new Finch();
-		Scanner f = new Scanner(System.in);
 		while (!myFinch.isFinchUpsideDown()) {
 			myFinch.setWheelVelocities(255, 255);
 			boolean[] obs = myFinch.getObstacleSensors();
@@ -35,15 +29,12 @@ public class FinchNav {
 			}
 			if (obs[0] && obs[1]) {
 				myFinch.stopWheels();
-		//		int rand = (int)Math.random()*2;
 				while (obs[0] && obs[1]) {
 					obs = myFinch.getObstacleSensors();
 
 					myFinch.setWheelVelocities(-255, -255,200);
 					
-			//		if (rand ==1)
 					myFinch.setWheelVelocities(255, -255,300);
-			//		else 
 
 				}
 
